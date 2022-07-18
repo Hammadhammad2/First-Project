a8b985c321d536853bf148711769a0953f5ab7b3
SalmanKinectro <salman.kinectro@gmail.com>
2022-07-18 18:17:38 +0500

    Final

--- Server/dbConnection.js
+++ Server/dbConnection.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import express from "express";
 
 export const dbConnection = () => {
+  const app = express();
   mongoose
     .connect(
       "mongodb+srv://Hammad:Hammadhammad1@cluster0.wa042.mongodb.net/authentication?retryWrites=true&w=majority",
@@ -10,8 +12,8 @@ export const dbConnection = () => {
       }
     )
     .then(() =>
-      app.listen(PORT, () =>
-        console.log(`Server Running on Port: http://localhost:${PORT}`)
+      app.listen(3001, () =>
+        console.log("Database Connected, Server Running on Port: 3001")
       )
     )
     .catch((error) => console.log(`${error} did not connect`));
